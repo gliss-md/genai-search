7e83860d4149f604d9439aa59c149043f1d627d6
Christian Glissmann <christian.glissmann@freenet.ag>
2025-12-08 20:04:47 +0100

    feat(conversation) init signal form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import {Routes} from '@angular/router';
-import {NewConversationViewComponent} from './feature/conversation/ui/new-conversation/new-conversation-view.component';
-import {ConversationViewComponent} from './feature/conversation/ui/conversation/conversation-view.component';
 
 export const routes: Routes = [
   {
--- src/app/feature/weekly-dashboard/add-task/add-task.ts
+++ src/app/feature/weekly-dashboard/add-task/add-task.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {Field, form} from '@angular/forms/signals';
+
+export interface Task {
+  name: string;
+  type: 'duration' | 'count' | 'sum' ;
+  goal: string;
+}
 
 @Component({
   selector: 'app-add-task',
-  imports: [],
+  imports: [
+    Field
+  ],
   templateUrl: './add-task.html',
   styleUrl: './add-task.scss',
 })
 export class AddTask {
+  private taskModel = signal<Task>({
+    name: '',
+    type: 'sum',
+    goal: '',
+  });
 
+  public taskForm = form(this.taskModel);
+
+  public submitTask(event: Event) {
+    event.preventDefault();
+    console.log(this.taskForm().value());
+  }
 }
