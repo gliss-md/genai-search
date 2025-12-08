ebb1b92f9e087ff1e1778795d15792634bfc7e13
Christian Glissmann <christian.glissmann@freenet.ag>
2025-12-08 19:41:59 +0100

    feat(conversation) init add task

--- src/app/feature/weekly-dashboard/weekly-dashboard.ts
+++ src/app/feature/weekly-dashboard/weekly-dashboard.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {AddTask} from './add-task/add-task';
 
 @Component({
   selector: 'app-weekly-dashboard',
-  imports: [],
+  imports: [
+    AddTask
+  ],
   templateUrl: './weekly-dashboard.html',
   styleUrl: './weekly-dashboard.css',
 })
